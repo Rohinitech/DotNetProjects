5f794bf55247132b173d63858d0c912503484e0e
Rohini <rohini16it@gmail.com>
2024-09-19 11:44:01 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace MyfirstApp{
+
+    class Program{
+        static void Main(string[] args){
+            
+           // string num = "233456";
+            //Console.WriteLine("Enter your number here:");
+            int myNum = 100 + 200;
+             Console.WriteLine("Hello my num:" + myNum);
+        } 
+    }
+}
